14414d0373011ce6d1ab0702e40256d0af09769c
Filippo <filippo.imarisio@validators.nl>
2021-07-02 16:42:51 +0200

    Renders a data table with stats

--- screens/Stats.tsx
+++ screens/Stats.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList} from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 import { useAppSelector } from '../hooks'
 import { DiscLocation } from '../types/types'
+import { DataTable } from 'react-native-paper';
+
 
 export default function Stats() {
   
   const discsLocation = useAppSelector((state) => state.discsLocation)
   
-  const renderItem = ({ item }: { item: DiscLocation }) => (
-    <View style={styles.item}>
-      <Text>{item.distanceFromEnd}</Text>
-    </View>
-  );
+
+  const renderItem = ({ item }: { item: DiscLocation }) => {
+    console.log('itemrender', item)
+    return (
+      <DataTable.Row key={item.id}>
+        <DataTable.Cell>Disc {item.id}</DataTable.Cell>
+        <DataTable.Cell numeric>{item.distanceFromEnd}</DataTable.Cell>
+      </DataTable.Row>
+    )
+  }
+  ;
 
   return (
     <View style={styles.container}>
       <Text>Stats</Text>
-      <FlatList
-        data={discsLocation}
-        renderItem={renderItem}
-        keyExtractor={item => item.id.toString()}
-      />
+      <DataTable> 
+        <DataTable.Header>
+          <DataTable.Title>Disc</DataTable.Title>
+          <DataTable.Title numeric>Distance</DataTable.Title>
+        </DataTable.Header>
+        { discsLocation.map(disc=>renderItem({item: disc}))}
+      </DataTable>
     </View>
   );
 }
@@ -28,14 +38,7 @@ export default function Stats() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#505050',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
+  }
 })
